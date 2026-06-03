fc3415fba9e4ff76cba0fbc9b851ca7a3d6251a0
FaveTD <taiwoadanini@gmail.com>
2026-06-03 13:40:29 +0100

    Refactor CalculateWinnings to use GameSettings

--- PremierLotto/PayoutCalc.cs
+++ PremierLotto/PayoutCalc.cs
@@ -1,18 +1,12 @@
-﻿using System;
+using System;
 
 namespace PremierLotto
 {
     public class PayoutCalculator
     {
-        public decimal CalculateWinnings(int matches, RiskLevel level, decimal stake)
+        public decimal CalculateWinnings(int matches, GameSettings settings, decimal stake)
         {
-            decimal multiplier = level switch
-            {
-                RiskLevel.Easy => 1.5m,   
-                RiskLevel.Classic => 3.0m, 
-                RiskLevel.Pro => 10.0m,    
-                _ => 1.0m
-            };
+            decimal multiplier = settings.Multiplier;
 
             decimal accuracyPercentage = matches / 4.0m;
 
