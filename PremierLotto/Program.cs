952512e08eec7ef163eedf9d8517c1c6c3ab3dfc
FaveTD <taiwoadanini@gmail.com>
2026-06-09 13:58:36 +0100

    Refactor program structure and enhance player data management
    Updated welcome message and refactored menu selection logic. Added ProfileDataManager for handling player data.

--- PremierLotto/Program.cs
+++ PremierLotto/Program.cs
@@ -10,26 +10,28 @@ namespace PremierLotto
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             "***************************************".WriteCentered(ConsoleColor.Yellow);
-            "** WELCOME TO PREMIER LOTTO 🤞       **".WriteCentered(ConsoleColor.Yellow);
+            "** WELCOME TO PREMIER Lotto 🤞       **".WriteCentered(ConsoleColor.Yellow);
             "***************************************".WriteCentered(ConsoleColor.Yellow);
 
             if (!GameStart.VerifyAgentAccess()) return;
 
-            GameOption selectedOption = GameSettings.ShowMenuAndSelect();
+            GameOption selectedOption = MenuManager.ShowMainMenuAndSelect();
 
             decimal userStake = GameStart.GetUserStake();
-
             bool allowDupes = GameSettings.GetDuplicatePreference();
-
             bool allowAlpha = GameSettings.GetAlphanumericPreference(selectedOption);
 
             GameSettings settings = new GameSettings(selectedOption, allowDupes, allowAlpha);
 
+            ProfileDataManager dataManager = new ProfileDataManager();
+
             Validation validator = new Validation();
             List<Player> playersList = new List<Player>();
-            GameStart.RegisterAgents(validator, playersList);
+            GameStart.RegisterAgents(validator, playersList, dataManager);
 
             TournamentManager.RunTournament(playersList, settings, userStake);
+
+            dataManager.UpdateAndSave(playersList);
 
             LeaderboardManager boardManager = new LeaderboardManager();
             boardManager.DisplayFinalResults(playersList, settings);
