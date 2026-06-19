4f3ff5d562a57c9dae24c74e931691ecdfa5d600
FaveTD <taiwoadanini@gmail.com>
2026-06-19 13:37:03 +0100

    Enhance Player class with Wallet and stake properties
    Added Wallet and ActiveRoundStake properties to Player class.

--- PremierLotto/Player.cs
+++ PremierLotto/Player.cs
@@ -1,28 +1,35 @@
+using PremierLotto.FInance;
 using System;
 using System.Collections.Generic;
 
-namespace PremierLotto
+namespace PremierLotto.Models
 {
     public class Player
     {
         public string RealName { get; set; }
-        public string PlayerAlias { get; set; } 
+        public string PlayerAlias { get; set; }
 
         public List<string> Guesses { get; set; }
 
         public int CorrectMatches { get; set; }
         public decimal TotalWinnings { get; set; }
-        
+
         public bool IsWinnerOfRound { get; set; }
 
-        public Player(string realName, string alias)
+        public Wallet Wallet { get; private set; }
+        public decimal ActiveRoundStake { get; set; }
+
+        public Player(string realName, string alias, Wallet profileWallet)
         {
             RealName = realName;
             PlayerAlias = alias;
             Guesses = new List<string>();
             CorrectMatches = 0;
             TotalWinnings = 0;
-            IsWinnerOfRound = false; 
+            IsWinnerOfRound = false;
+
+            Wallet = profileWallet;
+            ActiveRoundStake = 0.00m;
         }
     }
 }
