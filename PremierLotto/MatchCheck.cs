b3f2855f19835abd4a1b7050314632954aec62a0
FaveTD <taiwoadanini@gmail.com>
2026-06-19 13:26:35 +0100

    Refactor MatchCheck methods

--- PremierLotto/MatchCheck.cs
+++ PremierLotto/MatchCheck.cs
@@ -1,14 +1,37 @@
-﻿using System;
+using PremierLotto.Core;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
-namespace PremierLotto
+namespace PremierLotto.Game
 {
     public class MatchCheck
     {
-        public int GetMatchCount(List<string> userGuesses, List<string> winningNumbers)
+        public static int CountMatches(List<string> userGuesses, List<string> winningNumbers)
         {
+            if (userGuesses == null || winningNumbers == null) return 0;
             return userGuesses.Intersect(winningNumbers).Count();
+        }
+
+        public static bool EvaluatePassTarget(int matches, GameSettings settings)
+        {
+            int totalBallsPerRound = 4;
+            double matchPercentage = (double)matches / totalBallsPerRound;
+
+            if (settings.ModeName == "Easy")
+            {
+                return matchPercentage >= 0.25; 
+            }
+            if (settings.ModeName == "Classic")
+            {
+                return matchPercentage >= 0.50; 
+            }
+            if (settings.ModeName == "Pro")
+            {
+                return matchPercentage >= 0.75; 
+            }
+
+            return false;
         }
     }
 }
