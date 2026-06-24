ad6abcd516fd58323792dbc339715667c1924bcf
FaveTD <taiwoadanini@gmail.com>
2026-06-24 11:15:36 +0100

    Integrate FinanceManager and PoolManager into tournament

--- PremierLotto/Program.cs
+++ PremierLotto/Program.cs
@@ -2,6 +2,7 @@ using PremierLotto.Data;
 using PremierLotto.Game;
 using PremierLotto.Models;
 using PremierLotto.Utilities;
+using PremierLotto.Finance;
 using System;
 using System.Collections.Generic;
 
@@ -20,21 +21,22 @@ namespace PremierLotto.Core
             if (!GameStart.VerifyAgentAccess()) return;
 
             GameOption selectedOption = MenuManager.ShowMainMenuAndSelect();
-
             GameSettings settings = new GameSettings(selectedOption);
 
             ProfileDataManager dataManager = new ProfileDataManager();
             Validation validator = new Validation();
             List<Player> playersList = new List<Player>();
 
+            FinanceManager financeSystem = new FinanceManager();
+            PoolManager poolManager = new PoolManager(financeSystem); 
+
             GameStart.RegisterAgents(validator, playersList, dataManager);
 
-            TournamentManager.RunTournament(playersList, settings);
+            TournamentManager.RunTournament(playersList, settings, poolManager);
 
             dataManager.UpdateAndSave(playersList);
 
             LeaderboardManager boardManager = new LeaderboardManager();
-            boardManager.DisplayFinalResults(playersList, settings);
         }
     }
 }
